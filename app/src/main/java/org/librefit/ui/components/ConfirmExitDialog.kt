15f0d7713cd77e1e1d02e8635a79007e73cb0ac4
IamDg <180081466+IamDg@users.noreply.github.com>
2024-09-02 00:27:27 +0200

    Languages and hardcoded text in string.xml

--- app/src/main/java/org/librefit/ui/components/ConfirmExitDialog.kt
+++ app/src/main/java/org/librefit/ui/components/ConfirmExitDialog.kt
@@ -4,6 +4,8 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import org.librefit.R
 
 @Composable
 fun ConfirmExitDialog(
@@ -17,12 +19,12 @@ fun ConfirmExitDialog(
             TextButton(
                 onClick = onExit
             ){
-                Text(text = "Exit") /*TODO*/
+                Text(text = stringResource(id = R.string.label_exit_dialog))
             }
         },
         dismissButton = {
             TextButton(onClick = onDismiss ){
-                Text(text = "Cancel")/*TODO*/
+                Text(text = stringResource(id = R.string.label_cancel_dialog))
             }
         },
         text = { Text(text = text ) }
