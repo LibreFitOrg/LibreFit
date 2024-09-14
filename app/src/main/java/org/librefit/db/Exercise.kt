e0ffd3488ab7f57ab27ca57d10e3b16741f2c76b
IamDg <180081466+IamDg@users.noreply.github.com>
2024-09-15 00:17:48 +0200

    AGP upgrade

--- app/src/main/java/org/librefit/db/Exercise.kt
+++ app/src/main/java/org/librefit/db/Exercise.kt
@@ -2,6 +2,7 @@ package org.librefit.db
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
@@ -13,7 +14,8 @@ import androidx.room.PrimaryKey
             childColumns = ["workoutId"],
             onDelete = ForeignKey.CASCADE
         )
-    ]
+    ],
+    indices = [Index("workoutId")]
 )
 data class Exercise(
     @PrimaryKey(true) val id: Int = 0,
