aab175e3e63c482f57517337f5f47692ba3ca4b0
odweta <code.62rdl@aleeas.com>
2026-05-09 11:40:51 +0200

    fix: remove unused variables

--- app/src/main/java/org/librefit/ui/screens/settings/SettingsScreenViewModel.kt
+++ app/src/main/java/org/librefit/ui/screens/settings/SettingsScreenViewModel.kt
@@ -101,9 +101,6 @@ class SettingsScreenViewModel @Inject constructor(
         }
     }
 
-    private val _events = MutableSharedFlow<BackupEvent>()
-    val events = _events.asSharedFlow()
-
     fun backupExport(uri: Uri) {
         viewModelScope.launch {
             try {
