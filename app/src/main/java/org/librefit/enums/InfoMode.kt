61eb39f2b638f74808632f52f4b276910e8406b8
T1mothy <u2204430@pc.com>
2026-07-09 12:08:36 +0100

    feat: add Warmup Target and Type of Warmup information enums

--- app/src/main/java/org/librefit/enums/InfoMode.kt
+++ app/src/main/java/org/librefit/enums/InfoMode.kt
@@ -1,9 +1,10 @@
 /*
  * SPDX-License-Identifier: GPL-3.0-or-later
- * Copyright (c) 2024-2026. The LibreFit Contributors
+ * Copyright (c) 2025-2026. The LibreFit Contributors
  *
  * LibreFit is subject to additional terms covering author attribution and trademark usage;
  * see the ADDITIONAL_TERMS.md and TRADEMARK_POLICY.md files in the project root.
+ *
  */
 
 package org.librefit.enums
@@ -12,6 +13,9 @@ enum class InfoMode {
     DISMISS,
     REST_TIMER,
     TYPE_OF_SET,
+
+    WARMUP_TARGET,
+    TYPE_OF_WARMUP,
     BEFORE_SAVING_STATS,
     MUSCLE_DISTRIBUTION,
     EXERCISES_DISTRIBUTION,
