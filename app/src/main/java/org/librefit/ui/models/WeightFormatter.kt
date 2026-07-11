5ebf989fa369d9010387ad54586ff2871bca2385
IamDg <iamdg.dev@proton.me>
2026-07-11 18:05:15 +0200

    feat: add weight entity and related stuff
    - It should simplify the management of the unit system and values

---
+++ app/src/main/java/org/librefit/ui/models/WeightFormatter.kt
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (c) 2026. The LibreFit Contributors
+ *
+ * LibreFit is subject to additional terms covering author attribution and trademark usage;
+ * see the ADDITIONAL_TERMS.md and TRADEMARK_POLICY.md files in the project root.
+ */
+
+package org.librefit.ui.models
+
+import android.icu.text.MeasureFormat
+import android.icu.util.Measure
+import android.icu.util.MeasureUnit
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import org.librefit.enums.userPreferences.UnitSystem
+import org.librefit.models.Weight
+import org.librefit.nav.LocalUnitSystem
+import java.util.Locale
+
+@Composable
+fun Weight.formatToText(): String {
+    val unitSystem = LocalUnitSystem.current
+
+    return remember(this, unitSystem) {
+        // Automatically translates "kg"/"lbs" depending on device language
+        val format = MeasureFormat.getInstance(Locale.getDefault(), MeasureFormat.FormatWidth.SHORT)
+
+        if (unitSystem == UnitSystem.METRIC) {
+            format.format(Measure(inKilograms, MeasureUnit.KILOGRAM))
+        } else {
+            format.format(Measure(inPounds, MeasureUnit.POUND))
+        }
+    }
+}
+
+@Composable
+fun Weight.doubleValue(): Double {
+    val unitSystem = LocalUnitSystem.current
+
+    return this.doubleValue(unitSystem)
+}
+
+fun Weight.doubleValue(unitSystem: UnitSystem): Double {
+    return if (unitSystem == UnitSystem.METRIC) {
+        inKilograms
+    } else {
+        inPounds
+    }
+}
