05b767c1a376f66c772abb5dc4a41c40c06ee220
IamDg <iamdg.dev@proton.me>
2026-09-16 23:50:47 +0200

    fix: keep settings icon static

--- app/src/main/java/org/librefit/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/org/librefit/ui/screens/settings/SettingsScreen.kt
@@ -287,7 +287,7 @@ private fun SettingsScreenContent(
                             showConfirmDialogShowExerciseImages()
                         }
                     },
-                    icon = painterResource(if (showExercisesImages == true) R.drawable.ic_image else R.drawable.ic_hide_image),
+                    icon = painterResource(R.drawable.ic_image),
                     settingName = stringResource(R.string.show_images),
                     settingDesc = stringResource(if (showExercisesImages == true) R.string.show_images_desc else R.string.hide_images_desc)
                 )
